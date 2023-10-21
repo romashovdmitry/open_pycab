a6678707c8eaa6435a84a618d32d82915660b3b7
Romashov-Zima Dmitry <romashov.dmitry.py@gmail.com>
2023-10-22 00:05:24 +0400

    update yml

--- app/user/views.py
+++ app/user/views.py
@@ -34,8 +34,8 @@ class UserActions(ModelViewSet):
             refresh_token = RefreshToken.for_user(instance)
             response_data = {
                 "user": serializer.validated_data,
-                'access token': str(refresh_token.access_token),
-                'refresh token': str(refresh_token)
+                'access_token': str(refresh_token.access_token),
+                'refresh_token': str(refresh_token)
             }
             return Response(response_data, status=HTTP_201_CREATED)
         else:
