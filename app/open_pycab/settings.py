92c594f769dcff91157ca01042847b43e307f49f
Romashov-Zima Dmitry <romashov.dmitry.py@gmail.com>
2023-11-05 09:03:33 +0400

    django ssl update

--- app/open_pycab/settings.py
+++ app/open_pycab/settings.py
@@ -174,3 +174,5 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer"
 }
+
+SECURE_SSL_REDIRECT = True
